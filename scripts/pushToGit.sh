183fd0b5c876bb493194f726151b01a690fd33df
devatheboos <replayxexchangeco@gmail.com>
2025-04-13 16:51:39 +0530

    Update pushToGit.sh

--- scripts/pushToGit.sh
+++ scripts/pushToGit.sh
@@ -4,5 +4,9 @@ git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 
 git add posts/*.md public/images/*.png
-git commit -m "🤖 Auto-post: $(date)"
+
+# Only commit if there are changes
+git diff-index --quiet HEAD || git commit -m "🤖 Auto-post: $(date)"
+
+# Push using the custom-named secret
 git push "$REPO_PUSH_URL" HEAD:main
